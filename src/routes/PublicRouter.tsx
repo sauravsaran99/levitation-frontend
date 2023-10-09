2521bfe988df3aecf6364e59d6599c3b917e9d26
sauravsaran99 <sauravsaran99@gmail.com>
2023-10-09 17:30:38 +0530

    updadte minor

--- src/routes/PublicRouter.tsx
+++ src/routes/PublicRouter.tsx
@@ -5,7 +5,7 @@ import { Navigate } from "react-router-dom";
 const PublicRouter = ({ children }) => {
     const auth = useSelector((state: any) => state?.auth?.isAuthenticated);
   
-    return auth ? <Navigate to="/" replace />:children;
+    return auth ? <Navigate to="/userdetails" replace />:children;
   };
 
   export default PublicRouter;
